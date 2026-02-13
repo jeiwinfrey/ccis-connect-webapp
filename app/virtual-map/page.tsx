9db323ccabc8c34c3f64b08abfbb36e4e672cbb3
jeiwinfrey <jeiwinfreyulep12@gmail.com>
2026-02-14 00:54:35 +0800

    feat: add Tour component to virtual-map page

--- app/virtual-map/page.tsx
+++ app/virtual-map/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from "@/components/navbar";
+import Tour from "@/components/tour";
 
 export default function Page() {
   return (
     <div className="min-h-screen w-full bg-background">
       <Navbar />
+      <Tour />
     </div>
   );
 }
