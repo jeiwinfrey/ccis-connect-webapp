11a0f4727c9d69d17b37942f0d3c28779f9294e0
jeiwinfrey <jeiwinfreyulep12@gmail.com>
2026-02-14 00:36:42 +0800

    refactor: remove User component from virtual-map page

--- app/virtual-map/page.tsx
+++ app/virtual-map/page.tsx
@@ -1,11 +1,9 @@
 import Navbar from "@/components/navbar";
-import User from "@/components/user";
 
 export default function Page() {
   return (
     <div className="min-h-screen w-full bg-background">
       <Navbar />
-      <User />
     </div>
   );
 }
