d93ab7452136dfefc4c72284cf2ae67156e13069
jeiwinfrey <jeiwinfreyulep12@gmail.com>
2026-02-15 18:32:49 +0800

    feat: add borrow-equipment section

--- app/borrow/page.tsx
+++ app/borrow/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/navbar";
 import User from "@/components/user";
+import BorrowEquipment from "@/components/borrow-equipment";
 
 export default function Page() {
   return (
     <div className="min-h-screen w-full bg-background">
       <Navbar />
       <User />
+      <BorrowEquipment />
     </div>
   );
 }
