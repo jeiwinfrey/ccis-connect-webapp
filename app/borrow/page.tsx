e9a917029df50ab85e216fdc28518b12c69fabd4
jeiwinfrey <jeiwinfreyulep12@gmail.com>
2026-02-22 01:40:53 +0800

    chore: remove User component (subject to change)

--- app/borrow/page.tsx
+++ app/borrow/page.tsx
@@ -1,12 +1,10 @@
 import Navbar from "@/components/navbar";
-import User from "@/components/user";
 import BorrowEquipment from "@/components/borrow-equipment";
 
 export default function Page() {
   return (
     <div className="min-h-screen w-full bg-background">
       <Navbar />
-      <User />
       <BorrowEquipment />
     </div>
   );
--- app/reserve/page.tsx
+++ app/reserve/page.tsx
@@ -1,12 +1,10 @@
 import Navbar from "@/components/navbar";
-import User from "@/components/user";
 import ReserveRoom from "@/components/reserve-room";
 
 export default function Page() {
   return (
     <div className="min-h-screen w-full bg-background">
       <Navbar />
-      <User />
       <ReserveRoom />
     </div>
   );
